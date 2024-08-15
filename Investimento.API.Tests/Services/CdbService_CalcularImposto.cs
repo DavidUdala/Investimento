9ed22525e2a7ef6c5cd6fadc4f44336e8e8e9461
DavidUdala <david_udala@hotmail.com>
2024-08-15 12:09:38 -0300

    UPDATE: Implementado  testes sobre metodo privado CalcularImposto

--- Investimento.API.Tests/Services/CdbService_CalcularImposto.cs
+++ Investimento.API.Tests/Services/CdbService_CalcularImposto.cs
@@ -2,6 +2,7 @@
 using Investimento.API.Models;
 using Investimento.API.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Reflection;
 namespace Investimento.API.Tests.Services
 {
@@ -11,18 +12,25 @@ namespace Investimento.API.Tests.Services
         private readonly ICdbService _cdbService = new CdbService();
 
         [TestMethod]
-        public void RetornaValorImpostoQuandoCalculoRealiado()
+        [DataRow(100.00, 6, 105.98, 1.2)]
+        [DataRow(100.00, 7, 107.01, 1.4)]
+        [DataRow(100.00, 13, 113.4, 2.35)]
+        [DataRow(100.00, 25, 127.36, 4.10)]
+        public void RetornaValorImpostoQuandoImpostoCalculadoRealizado(double valorMonetario, int prazoEmMeses, double valorBrutoTotal, double impostoEsperado)
         {
             //Arrange
             var methodInfo = typeof(CdbService).GetMethod("CalcularImposto", BindingFlags.NonPublic | BindingFlags.Instance);
 
             if (methodInfo == null)
                 Assert.Fail("Método privado não encontrado.");
+
+            var investimentoRequest = new InvestimentoRequest(valorMonetario, prazoEmMeses);
             //Act
-            var result = (double)methodInfo.Invoke(_cdbService, new object[] {new InvestimentoRequest(100,6), 100 }); 
+            double result = (double)methodInfo.Invoke(_cdbService, new object[] { investimentoRequest, valorBrutoTotal });
 
             //Assert
             Assert.IsNotNull(result);
+            Assert.AreEqual(impostoEsperado, Math.Round(result, 2));
         }
 
     }
